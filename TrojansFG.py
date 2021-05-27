96ce3236808dd6afa1d6f5296df18000324338b2
FFM600-31 <84896154+FFM600-31@users.noreply.github.com>
2021-05-27 18:03:48 +0700

    Add files via upload

---
+++ TrojansFG.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import time
+
+os.system('clear')
+print ('\033[1;32m')
+Menu = """
+---------------VirusTrojans---------------
+[+] Author By FFM. Yt
+[+] Youtube : FFM. YT
+[+] Github : https://github.com/FFM600-31
+------------------------------------------
+"""
+print Menu
+print ('\033[1;33m'"Ketik CTRL+Z Untuk memberentikan Virus")
+No = raw_input('\033[1;33m'"Ketik Nomor WA : ")
+nanya = raw_input("starting Attack Y/N : ")
+if nanya == 'Y' or nanya == 'y':
+     print("Starting Attack")
+     time.sleep(1)
+     i = 1
+     while i <= 5:
+           print('\033[1;33m'"Menyerang Hp Korban ->"'\033[37;1m'),No
+if nanya == 'N' or nanya == 'n':
+ time.sleep(1)
+print ('\033[37;1m')
+os.system('login')
